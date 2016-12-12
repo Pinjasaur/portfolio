3aac75f07fb2387c2e3a4488c4ae71bc4b46be98
Paul Esch-Laurent <herppfel@gmail.com>
2016-12-11 23:41:09 -0500

    [js] Randomize the 'name' and 'message' form placeholders

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,35 @@
 (function() {
   "use strict";
 
+  // animate moving between anchor links
   smoothScroll.init({
     selector: "a",
     speed: 500,
     easing: "easeInOutCubic"
   });
 
+  // better external SVG spiresheet support
   svg4everybody();
+
+  // random placeholders for the contact form fields
+  var form = document.querySelector(".contact"),
+      names = [
+        "Paul Bunyan",
+        "Luke Skywalker",
+        "Jason Bourne",
+        "James Bond"
+      ],
+      messages = [
+        "Like what you see? Let me know!",
+        "Want to know more? Get in touch!",
+        "Hey! Did I tickle your fancy?"
+      ];
+
+  form.querySelector("[id='name']").placeholder = randomFromArray(names);
+  form.querySelector("[id='message']").placeholder = randomFromArray(messages);
+
+  function randomFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
 
 })();
